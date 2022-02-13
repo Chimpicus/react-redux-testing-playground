42b40037c222b3c1ad375e7fbe7f31f274ec97af
Chimpicus <caseyboyes@gmail.com>
2022-02-13 11:53:59 +0000

    typescript fizzbuzz built

---
+++ src/typescript/fizzbuzz.js
@@ -0,0 +1,21 @@
+//Fizz Buzz built with typescript that is also an Imediately Invoked Function Expression (IIFE)
+(function () {
+    var output = [];
+    var i = null;
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            output.push("FizzBuzz");
+        }
+        else if (i % 3 === 0) {
+            output.push("Fizz");
+        }
+        else if (i % 5 === 0) {
+            output.push("Buzz");
+        }
+        else {
+            output.push(i);
+        }
+    }
+    // eslint-disable-next-line
+    console.log(output);
+})();
